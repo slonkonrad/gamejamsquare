734eec086a3d5aa3e917c3273ab56a559a628f8b
Konrad Słoń <slonkonrad@gmail.com>
2019-04-27 00:31:42 +0200

    joystick inputs implementation

---
+++ jamsquare/Assets/_Scripts/Inputs/BaseInput/BaseInput.cs
@@ -0,0 +1,8 @@
+﻿[System.Serializable]
+public class BaseInput
+{
+    public enum PlayerKey {one = 0, two = 1 }
+    public PlayerKey playerKey;
+
+    public int PlayerID { get { return (int)playerKey; } }
+}
