b7d597efb37714353ef0a937802d9c123cd9e5c6
Tabisz <maro.990@gmail.com>
2019-04-27 01:27:02 +0200

    HumanBehavior first

--- jamsquare/Assets/_Scripts/HumanBehavior/Human.cs
+++ jamsquare/Assets/_Scripts/HumanBehavior/Human.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(Rigidbody))]
 public class Human : MonoBehaviour
 {
     Rigidbody rb;
-    Transform target;
+    public Target currentTarget;
+    [SerializeField]
+    private float speed = 2;
+
+    public float StartRandomValue = 10;
+
+    
 
     void Start()
     {
@@ -13,26 +19,48 @@ public class Human : MonoBehaviour
     }
     void Initialise()
     {
-        rb.GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        if (!currentTarget.isStartTarget)
+            Debug.Log("to niee jest startowy target");
+
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        CustomUpdate();
+        CustomFixedUpdate();
     }
-    void CustomUpdate()
+    void CustomFixedUpdate()
     {
+        GoToTarget(currentTarget.transform);
+        if (Vector3.Distance(transform.position, currentTarget.transform.position) < currentTarget.range)
+            if (currentTarget.GetNextTarget() != null)
+            currentTarget = currentTarget.GetNextTarget();
 
     }
 
-    void GoToTarget()
+    void GoToTarget(Transform target)
     {
-
+        Vector3 pos = target.position;
+        Move(pos);
+        Debug.DrawLine(transform.position, target.transform.position,Color.red);
     }
 
-    void Move(Transform target)
+    [SerializeField]
+    private float maxVelocity;
+    [SerializeField]
+    private float acceleration;
+
+    public float DEBUG_currentVelocity;
+    void Move(Vector3 target)
     {
-        rb.AddForce(transform.position - target.position);
+        //rb.velocity += (target - transform.position) * speed*Time.fixedDeltaTime;
+        float lerpValue = ((maxVelocity - rb.velocity.magnitude) / maxVelocity) * acceleration;
+        if (rb.velocity.magnitude >= maxVelocity)
+            lerpValue = 1;
+        rb.velocity = Vector3.Normalize((target - transform.position) )* Mathfx.Lerp(rb.velocity.magnitude, speed, lerpValue);
+        DEBUG_currentVelocity = rb.velocity.magnitude;
     }
+
+
 
 }
