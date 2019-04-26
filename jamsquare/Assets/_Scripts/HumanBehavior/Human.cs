e40600ac1b98285bbf4ba5c6d9cf7b989c1e6fc4
Tabisz <maro.990@gmail.com>
2019-04-26 21:53:21 +0200

    Test Materials

---
+++ jamsquare/Assets/_Scripts/HumanBehavior/Human.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+    Rigidbody rb;
+    Transform target;
+
+    void Start()
+    {
+        Initialise();
+    }
+    void Initialise()
+    {
+        rb.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        CustomUpdate();
+    }
+    void CustomUpdate()
+    {
+
+    }
+
+    void GoToTarget()
+    {
+
+    }
+
+    void Move(Transform target)
+    {
+        rb.AddForce(transform.position - target.position);
+    }
+
+}
